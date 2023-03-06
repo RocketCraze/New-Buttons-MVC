2809f396afe78875d39121e42b50bf7f65c980d7
RocketCraze <102596494+RocketCraze@users.noreply.github.com>
2023-03-06 13:55:40 +0200

    Added new button page
    New page was added with two buttons

---
+++ Graduate Project - Leon/Controllers/ButtonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Graduate_Project___Leon.Controllers
+{
+    public class ButtonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
