b4a3246c0739a32fa9fce33c76127eadf8392fe9
RocketCraze <102596494+RocketCraze@users.noreply.github.com>
2023-03-06 11:19:58 +0200

    Adding Default MVC Files to Git

---
+++ Graduate Project - Leon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Graduate_Project___Leon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
